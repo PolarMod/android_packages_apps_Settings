5dd871ba15b558193e6392f0109bbad29aa2be89
Andrewerr <toddot@protonmail.com>
2022-08-17 20:42:06 +0300

    [settings] [custom settings] [icons] [themes]: disabled duplictating adaptive icon shape customization

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -91,8 +91,8 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.font"));
-        controllers.add(new OverlayCategoryPreferenceController(context,
-                "android.theme.customization.adaptive_icon_shape"));
+        //controllers.add(new OverlayCategoryPreferenceController(context,
+        //        "android.theme.customization.adaptive_icon_shape"));
         return controllers;
     }
 
